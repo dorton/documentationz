89cc448a58890905e991575a150923f73afece73
Brian Dorton <dorton@gmail.com>
2014-11-21 22:16:25 -0600

    first commit

---
+++ app/views/teachers/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @teacher, :id, :last_name, :first_name, :created_at, :updated_at
---
+++ db/migrate/20141120181039_create_teachers.rb
@@ -0,0 +1,10 @@
+class CreateTeachers < ActiveRecord::Migration
+  def change
+    create_table :teachers do |t|
+      t.string :last_name
+      t.string :first_name
+
+      t.timestamps
+    end
+  end
+end
